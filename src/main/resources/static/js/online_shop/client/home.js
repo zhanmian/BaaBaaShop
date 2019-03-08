6c3c890653452dd6ef16597278ccbd924c3deac5
zhanmian <zhanmian@yeah.net>
2019-03-08 11:20:29 +0800

    压缩图片

--- src/main/resources/static/js/online_shop/client/home.js
+++ src/main/resources/static/js/online_shop/client/home.js
@@ -277,7 +277,7 @@ $(document).ready(function()
 	            {
 	            	price: function(itemElement)
 	            	{
-	            		var priceEle = $(itemElement).find('.product_price').text().replace( '$', '' );
+	            		var priceEle = $(itemElement).find('.product_price').text().replace( '￥', '' );
 	            		return parseFloat(priceEle);
 	            	},
 	            	name: '.product_name',
